457f08df9a1a24af0876fb7059eaba1eece71486
Luisa Castaño <luisa.castano40@eia.edu.co>
2024-04-18 06:28:53 -0500

    [update] servidor

---
+++ app.js
@@ -0,0 +1,25 @@
+//1. IMPORTAR LAS DEPENDENCIAS QUE VAMOS A USAR
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+
+
+
+//2. CONFIGURAR NUESTRO SERVIDOR
+const app = express();
+const puerto = 9000;
+
+//2.1 configurar las variables de entorno
+dotenv.config();
+
+
+// 3. ESTABLECER LA CONECCION CON NUESTRO FRONT
+
+
+
+//4.INICIALIZAMOS EL SERVIDOR
+
+app.listen(puerto, () => {
+    console.log(`El servidor está escuchando en http://localhost:${puerto}`); 
+});
+
