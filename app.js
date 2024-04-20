d0cae7a2f65d046ea79384faf80cff7641ee5008
Luisa Castaño <luisa.castano40@eia.edu.co>
2024-04-19 20:41:57 -0500

    ENTREGA 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
-
+import conexionMongo from './src/config/baseDatos.js';
 
 
 //2. CONFIGURAR NUESTRO SERVIDOR
@@ -12,9 +12,21 @@ const puerto = 9000;
 //2.1 configurar las variables de entorno
 dotenv.config();
 
+//2.2 configurar conexion base de datos
+conexionMongo();
 
-// 3. ESTABLECER LA CONECCION CON NUESTRO FRONT
 
+// 3. ESTABLECER LA CONEXIÓN CON NUESTRO FRONT
+const rutaPublica = path.join(process.cwd(), 'public');
+
+app.use(express.static(rutaPublica));
+
+app.use(express.json());
+
+//Especificamos que vamos a acceder a nuestro index.html
+app.get('/', (req,res) => {
+    res.sendFile(path.join(rutaPublica,'index.html' ))
+});
 
 
 //4.INICIALIZAMOS EL SERVIDOR
